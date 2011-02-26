498f4cddec750654a8619eb284ee1489fc459744
priceputu.cristian <priceputu.cristian@localhost>
2011-02-26 15:34:00 +0100

    Parser begin.

--- src/gotpl/gotpl_parsers.h
+++ src/gotpl/gotpl_parsers.h
@@ -1,8 +1,13 @@
 #include "gotpl.h"
+#include "gotpl_pool.h"
 
 #ifndef __GOTPL_PARSERS__H
 #define __GOTPL_PARSERS__H
 
+typedef struct gotpl_parser gotpl_parser;
+
 gotpl_bool gotpl_parser_is_cwhitespace(gotpl_ci utf8Char);
+
+gotpl_parser* gotpl_utf8parser_create(gotpl_pool* pool);
 
 #endif
