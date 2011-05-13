2c81d77c9ab52f1f7235ae69ca44a4bc449fcef1
priceputu.cristian <priceputu.cristian@localhost>
2011-05-13 23:39:45 +0200

    Added memory detection.

--- src/common.c
+++ src/common.c
@@ -23,6 +23,48 @@ u16int inw(u16int port)
     return ret;
 }
 
+u32int strlen(const char *string) {
+	const char *s;
+
+	s = string;
+	while (*s)
+		s++;
+	return s - string;
+}
+
+char *strrev(char *str) {
+	char *p1, *p2;
+
+	if (!str || !*str)
+		return str;
+
+	for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2) {
+		*p1 ^= *p2;
+		*p2 ^= *p1;
+		*p1 ^= *p2;
+	}
+
+	return str;
+}
+
+char *itoa(int n, char *s, int b) {
+	static char digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+	int i=0, sign;
+    
+	if ((sign = n) < 0)
+		n = -n;
+
+	do {
+		s[i++] = digits[n % b];
+	} while ((n /= b) > 0);
+
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+
+	return strrev(s);
+}
+
 // Copy len bytes from src to dest.
 void memcpy(u8int *dest, const u8int *src, u32int len)
 {
--- src/main.c
+++ src/main.c
@@ -2,13 +2,16 @@
 //           Made for JamesM's tutorials <www.jamesmolloy.co.uk>
 
 #include "monitor.h"
+#include "common.h"
 
 int main(struct multiboot *mboot_ptr)
 {
+    char buf[20];
     // Initialise the screen (by clearing it)
     monitor_clear();
     // Write out a sample string
-    monitor_write("Hello, world!");
+    monitor_write("Hello, world!\n");
+    monitor_write(itoa(mboot_ptr->mem_upper,buf,10));
     
     return 0;
 }
