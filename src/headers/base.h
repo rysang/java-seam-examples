46c6b1b7013b58f904abcffb4ac62af34d702a7f
priceputu.cristian <priceputu.cristian@localhost>
2011-05-14 14:47:38 +0200

    Added basic boot sequence.

---
+++ src/headers/base.h
@@ -0,0 +1,11 @@
+#ifndef BASE_H_
+#define BASE_H_
+
+typedef unsigned int ui32;
+typedef int i32;
+typedef unsigned short ui16;
+typedef short i16;
+typedef unsigned char ui8;
+typedef char i8;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "headers/boot/boot.h"
+
+int main(multiboot *mboot_ptr) {
+
+	return 0;
+}
