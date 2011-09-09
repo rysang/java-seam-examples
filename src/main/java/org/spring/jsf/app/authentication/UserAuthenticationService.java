cd0eba2d7ec781987c48b4decc69b3e941d1f8f9
price <price@localhost>
2011-09-09 12:11:09 +0200

    Latest custom login.

--- src/main/java/org/spring/jsf/app/authentication/UserAuthenticationService.java
+++ src/main/java/org/spring/jsf/app/authentication/UserAuthenticationService.java
@@ -4,35 +4,30 @@ import java.util.Arrays;
 
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Scope;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service("userDetailsService")
 @Scope("prototype")
-public class UserAuthenticationService implements AuthenticationProvider {
+public class UserAuthenticationService implements UserDetailsService {
 
   private static final Logger LOG = Logger.getLogger("userDetailsService");
 
-  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-    UsernamePasswordAuthenticationToken ret = null;
+  public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException, DataAccessException {
+    User user = new User("moez", "moez", true, true, true, true, Arrays.asList(new GrantedAuthority[] {
+        new Authority("ROLE_ADMIN"), new Authority("ROLE_ANONYMOUS") }));
 
-    if (authentication instanceof UsernamePasswordAuthenticationToken) {
-      ret = new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials(),
-          Arrays.asList(new GrantedAuthority[] { new Authority("ROLE_ADMIN"), new Authority("ROLE_ANONYMOUS") }));
-    }
-
-    return ret;
-  }
-
-  public boolean supports(Class<? extends Object> authentication) {
-    if (authentication.getName().equals(UsernamePasswordAuthenticationToken.class.getName()))
-      return true;
-
-    return false;
+    return user;
   }
 
 }
