9dd4991cc802c3b97e12af498c7ead54a851c232
price <price@localhost>
2011-09-23 16:30:51 +0200

    Initial files.

--- src/main/java/org/cpcs/navigation/api/NavigationService.java
+++
@@ -1,9 +0,0 @@
-package org.cpcs.navigation.api;
-
-public interface NavigationService {
-  public String navigateTo(String navigation, String title);
-
-  public String getCurrentPage();
-
-  public String getCurrentPageTitle();
-}
