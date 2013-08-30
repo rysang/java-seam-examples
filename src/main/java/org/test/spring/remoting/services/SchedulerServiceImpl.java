c875cda72a71e2920e79b33f494cddb9652ad625
priceputu.cristian <priceputu.cristian@localhost>
2013-08-30 23:30:31 +0300

    df

--- src/main/java/org/test/spring/remoting/services/SchedulerServiceImpl.java
+++ src/main/java/org/test/spring/remoting/services/SchedulerServiceImpl.java
@@ -2,39 +2,45 @@ package org.test.spring.remoting.services;
 
 import java.rmi.RemoteException;
 
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 import org.test.spring.remoting.services.api.ResultListener;
 import org.test.spring.remoting.services.api.SchedulerService;
 import org.test.spring.remoting.services.api.TaskType;
 
-public class SchedulerServiceImpl implements SchedulerService {
+public class SchedulerServiceImpl implements SchedulerService, ApplicationListener {
 
-	public SchedulerServiceImpl() {
+    public SchedulerServiceImpl() {
 
-	}
+    }
 
-	@Override
-	public long getRunningThreads() throws RemoteException {
-		return 0;
-	}
+    @Override
+    public long getRunningThreads() throws RemoteException {
+        return 0;
+    }
 
-	@Override
-	public long getTotalThreads() throws RemoteException {
+    @Override
+    public long getTotalThreads() throws RemoteException {
 
-		return 0;
-	}
+        return 0;
+    }
 
-	@Override
-	public long getMaximumRunningThreads() throws RemoteException {
+    @Override
+    public long getMaximumRunningThreads() throws RemoteException {
 
-		return 0;
-	}
+        return 0;
+    }
 
-	@Override
-	public void runTask(TaskType type, ResultListener listener,
-			Object... params) throws RemoteException {
+    @Override
+    public void runTask(TaskType type, ResultListener listener, Object... params) throws RemoteException {
 
-		if (listener != null) {
-			listener.onResult("Hey man !!!!");
-		}
-	}
+        if (listener != null) {
+            listener.onResult("Hey man !!!!");
+        }
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        System.out.println("EVENT: " + event);
+    }
 }
